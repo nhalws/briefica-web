5b2cf798b5f3e60c1bf71f3bc1930ff9e3f97677
noah williams <williams@noahs-MacBook-Pro.local>
2026-01-13 07:57:57 -0800

    Update downloads page

--- app/downloads/page.tsx
+++ app/downloads/page.tsx
@@ -177,7 +177,7 @@ export default function DownloadsPage() {
                 3
               </div>
               <div>
-                <p className="font-medium text-white mb-1">drag to your Desktop</p>
+                <p className="font-medium text-white mb-1">drag briefica to your Desktop</p>
                 <p className="text-sm">drag briefica to your destination folder and update the path.
                   NOTE: that briefica initially creates a /briefsets/ folder on your desktop where all sets must be stored to load.</p>
               </div>
